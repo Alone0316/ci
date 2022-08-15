4c11e9c95f8fc6782959113d7ae92277c5ca4fc2
Alone0316 <70204635+Alone0316@users.noreply.github.com>
2022-08-15 14:14:01 +0530

    trial

--- build.sh
+++ build.sh
@@ -181,6 +181,8 @@ fi
 
 mkdir -p out
 
+make O=out clean && make O=out mrproper
+
 make -C $(pwd) O=$(pwd)/out DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y CLANG_TRIPLE=aarch64-linux-gnu- vendor/a52sxq_chn_tw_defconfig
 make -C $(pwd) O=$(pwd)/out DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y CLANG_TRIPLE=aarch64-linux-gnu-
 
